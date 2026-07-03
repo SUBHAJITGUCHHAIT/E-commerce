a687ab2d58c893f13cf278d25c4dff0fcdf1b4ca
Subhajit Guchhait <subhajitguchhait12@gmail.com>
2026-07-03 07:39:27 +0530

    Update Product Image

--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -10,8 +10,14 @@ import com.ecommerce.project.repositories.ProductRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -107,5 +113,44 @@ public class ProductServiceImpl implements ProductService {
                 .orElseThrow(()-> new ResourceNotFoundException("Product", "productId", productId));
         productRepository.delete(product);
         return modelMapper.map(product, ProductDTO.class);
+    }
+
+    @Override
+    public ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException {
+
+        Product productFromDB = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "product", productId));
+
+        String path = System.getProperty("user.dir") + File.separator + "images";
+
+        String fileName = uploadImage(path, image);
+
+        productFromDB.setImage(fileName);
+
+        Product updatedProduct = productRepository.save(productFromDB);
+
+        return modelMapper.map(updatedProduct, ProductDTO.class);
+    }
+
+    private String uploadImage(String path, MultipartFile file) throws IOException {
+
+        String originalFileName = file.getOriginalFilename();
+
+        String randomId = UUID.randomUUID().toString();
+
+        String fileName = randomId +
+                originalFileName.substring(originalFileName.lastIndexOf('.'));
+
+        File folder = new File(path);
+
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        String filePath = Paths.get(path, fileName).toString();
+
+        Files.copy(file.getInputStream(), Paths.get(filePath));
+
+        return fileName;
     }
 }
